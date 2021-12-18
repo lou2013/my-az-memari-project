ef3ad736718ac7b351ee1a9fd57789b43a255d57
lou2013 <imaniman1377@yahoo.com>
2021-12-18 21:18:47 +0330

    mmm

--- server.js
+++ server.js
@@ -1,40 +1,1 @@
-const express = require('express');
-const axios = require('axios');
-const {Agent} = require('https');
-const app = express();
-app.get('/', (req, res) => res.send(`Hello world here and now to test this http server running on raspberry pi port 4000<br/>click <a href="/page"> here </a> for page<br/>
-click <a href="/about">here</a> for creators<br>
-<a href="/sms">sms</a>`));
-app.get('/page', (req, res) => res.send('this is another page just to test the http server running on raspberry pi port 4000 <br/> click <a href="/"> here </a> for home'));
-app.get('/about', (req, res) => res.send('iman moghadari(9740503) and yegane mohammadi(96837018) are the creators of this server <br/> click <a href="/"> here </a> for home'));
-app.get('/sms', (req, res) => res.send(`this is the send sms page <br>
-<form action="/send" action="#" method="POST" >
-<input type="submit" value="Submit">
-    </form>
-`));
-app.post('/send', async (req, res) => {
-const axi = new axios.Axios({baseURL:'https://services.avapardaz.org/v1/sms/send',    httpsAgent: new Agent({
-    rejectUnauthorized: false,
-  }),
-  responseType: 'json',
-  transformResponse: (res)=> {
-    return JSON.parse(res);
-  },
-  transformRequest: (req)=> {
-    return JSON.stringify(req);
-},
-headers:{
-['Content-Type']: 'application/json',
-['token']:"1715cb3b85a36f8a55b8ddbb54370ecfe1bedb7e877514b3abd2cb2c51d34bca6eabc7195aff9d6cf9932f295489559f135665b0316f4c259d7968f620ca6414dacd5a92537e13eaa0870f6ac8cc25ce41db22a6e99ae52de19e1ca4268b38d93cc2ec33b36818c068f3c94215e518aefab9df71e2d568168ef11dc79d5e6a24"
-},})
-    const d = await axi.post('',{
-            "receptor": [
-              "09360074042"
-            ],
-            "message": "this is a message to test kaveNegar",
-    });
-    res.send(d.data);
-});
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => console.log("App listening at port:", port))
+const express = require('express');const axios = require('axios');const {Agent} = require('https');const app = express();app.get('/', (req, res) => res.send(`Hello world here and now to test this http server running on raspberry pi port 4000<br/>click <a href="/page"> here </a> for page<br/>click <a href="/about">here</a> for creators<br><a href="/sms">sms</a>`));app.get('/page', (req, res) => res.send('this is another page just to test the http server running on raspberry pi port 4000 <br/> click <a href="/"> here </a> for home'));app.get('/about', (req, res) => res.send('iman moghadari(9740503) and yegane mohammadi(96837018) are the creators of this server <br/> click <a href="/"> here </a> for home'));app.get('/sms', (req, res) => res.send(`this is the send sms page <br><form action="/send" action="#" method="POST" ><input type="submit" value="Submit">    </form>`));app.post('/send', async (req, res) => {const axi = new axios.Axios({baseURL:'https://services.avapardaz.org/v1/sms/send',    httpsAgent: new Agent({    rejectUnauthorized: false,  }),  responseType: 'json',  transformResponse: (res)=> {    return JSON.parse(res);  },  transformRequest: (req)=> {    return JSON.stringify(req);},headers:{['Content-Type']: 'application/json',['token']:"f6d8dfdde9d6f6c72a7771c45e46975930096a0d65188351591db1c3c52f81d2fa37423cdbd46fa75b26aaf3fa82c578db8c3757542378e9443fe5576f77c892e4dfa46c71f5b83fc56e9abba80653f63d8b5a5c43d0e01777d0e5c5273f3e9a2b547689a001f2656545ebc0ca3f3e4a2556b8a33836cfd54e2baf4d878834ab"},})    const d = await axi.post('',{            "receptor": [              "09360074042"            ],            "message": "this is a message to test kaveNegar",    });    res.send(d.data);});const port = process.env.PORT || 4000;app.listen(port, () => console.log("App listening at port:", port))
